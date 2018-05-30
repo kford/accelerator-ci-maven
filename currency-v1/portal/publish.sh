756ca479080bd8254483ccd95038c563e97c0a6e
Kevin Ford <kevinford@google.com>
2018-05-30 06:48:58 -0700

    Update Portal Publishgin ETag lookup

--- currency-v1/portal/publish.sh
+++ currency-v1/portal/publish.sh
@@ -34,7 +34,7 @@ curl -i -X PUT "https://e2e.apigee.net/c3Rvc-ZG9j-1822/content" \
    -H "Authorization: Bearer $ACCESS_TOKEN" \
    -H "X-Org-Name: kevinford-eval" \
    -H "Accept: application/json, text/plain, */*" \
-   -H 'X-Requested-With: XMLHttpRequest' \
+   -H "X-Requested-With: XMLHttpRequest" \
    -H "If-Match: $ETAG_BAGGAGE" \
    -H "Content-Type:application/x-yaml" \
    -d @baggage-spec.json
@@ -44,7 +44,7 @@ curl -i -X PUT "https://e2e.apigee.net/c3Rvc-ZG9j-1823/content" \
    -H "Authorization: Bearer $ACCESS_TOKEN" \
    -H "X-Org-Name: kevinford-eval" \
    -H "Accept: application/json, text/plain, */*" \
-   -H 'X-Requested-With: XMLHttpRequest' \
+   -H "X-Requested-With: XMLHttpRequest" \
    -H "If-Match: $ETAG_FLIGHTS" \
    -H "Content-Type:application/x-yaml" \
    -d @flights-spec.json
