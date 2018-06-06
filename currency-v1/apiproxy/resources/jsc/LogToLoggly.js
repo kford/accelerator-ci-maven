50a4d5957163e18244fb35742c680fb0a83fdae9
Kevin Ford <kevinford@google.com>
2018-06-06 12:05:21 -0700

    Remove system property sys:=apigee-apim

--- currency-v1/apiproxy/resources/jsc/LogToLoggly.js
+++ currency-v1/apiproxy/resources/jsc/LogToLoggly.js
@@ -5,7 +5,6 @@ var logglyUrl = logglyUrlScheme + '://' + logglyUrlDomain + logglyBasePath + '/l
 context.setVariable('debug.logglyUrl', logglyUrl);
 
 var log = {
-	sys: 'apigee-apim',
 	org: context.getVariable('organization.name'),
 	env: context.getVariable('environment.name'),
 	api: context.getVariable('apiproxy.name'),
