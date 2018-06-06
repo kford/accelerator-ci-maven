397b5e6f8382c8530c9d77c15e422dddd3d5bba2
Kevin Ford <kevinford@google.com>
2018-06-06 10:57:12 -0700

    Update logs

--- currency-v1/apiproxy/resources/jsc/LogToLoggly.js
+++ currency-v1/apiproxy/resources/jsc/LogToLoggly.js
@@ -5,7 +5,7 @@ var logglyUrl = logglyUrlScheme + '://' + logglyUrlDomain + logglyBasePath + '/l
 context.setVariable('debug.logglyUrl', logglyUrl);
 
 var log = {
-	sys: "apigee-apim",
+	system: "apigee-apim",
 	org: context.getVariable('organization.name'),
 	env: context.getVariable('environment.name'),
 	api: context.getVariable('apiproxy.name'),
