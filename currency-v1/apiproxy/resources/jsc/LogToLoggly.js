857470fa8c731796957703303f30d484d60af641
Kevin Ford <kevinford@google.com>
2018-06-06 10:58:28 -0700

    Logging Changes

--- currency-v1/apiproxy/resources/jsc/LogToLoggly.js
+++ currency-v1/apiproxy/resources/jsc/LogToLoggly.js
@@ -5,7 +5,7 @@ var logglyUrl = logglyUrlScheme + '://' + logglyUrlDomain + logglyBasePath + '/l
 context.setVariable('debug.logglyUrl', logglyUrl);
 
 var log = {
-	system: "apigee-apim",
+	correlationId: context.getVariable('messageid'),
 	org: context.getVariable('organization.name'),
 	env: context.getVariable('environment.name'),
 	api: context.getVariable('apiproxy.name'),
